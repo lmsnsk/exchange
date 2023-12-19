f6375dc90d463aa0ed11867ba9f901308e952ccc
glengunt <lmslms87@yandex.ru>
2023-12-19 13:26:46 +0700

    3

--- s21_decimal.c
+++ s21_decimal.c
@@ -1,34 +1,63 @@
 #include "s21_decimal.h"
 
 int main() {
-  printf("%lu\n",
-         0b1111111111111111111111111111111111111111111111111111111111111111);
+  s21_decimal example;
+  decimal_zero(&example);
+  s21_from_int_to_decimal(500, &example);
+
+  printf("%u\n", example.bits[0]);
+  printf("%u\n", example.bits[1]);
+  printf("%u\n", example.bits[2]);
+  printf("%u\n", example.bits[3]);
   return 0;
 }
 
-void s21_decimal_zero(s21_decimal *dst) {
+void decimal_zero(s21_decimal *dst) {
   for (int i = 0; i < 4; i++) dst->bits[i] = 0;
 }
 
+void invert_sign(s21_decimal *dst) {
+  if (dst->bits[3] == 0) {
+    dst->bits[3] = 1;
+    dst->bits[3] <<= 31;
+  } else
+    dst->bits[3] = ~(dst->bits[3]) + 1;
+}
+
+int get_sign(s21_decimal dst) { return (dst.bits[3] & MASK) >> 31; }
+
+int get_bit(s21_decimal dst, unsigned pos) {
+  int bit = -1;
+  int val = pow(2, pos);
+  if (pos < 32) {
+    bit = (dst.bits[0] & val) >> pos;
+  } else if (32 >= pos && pos < 64) {
+    bit = (dst.bits[1] & val) >> pos;
+  } else if (pos < 96) {
+    bit = (dst.bits[2] & val) >> pos;
+  } else if (pos < 128) {
+    bit = (dst.bits[3] & val) >> pos;
+  }
+  return bit;
+};
+
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
-  int code = 0;
-  s21_decimal_zero(dst);
+  int code = 0, sign = 0;
+  decimal_zero(dst);
   if (src < 0) {
-    s21_invert_sign(dst);  // sign -
     src = -src;
+    sign = 1;
   }
   if (src > __INT_MAX__) code = 1;
   if (!code) dst->bits[0] = src;
+  if (sign) invert_sign(dst);
   return code;
 }
 
-void s21_invert_sign(s21_decimal *dst) { dst->bits[3] *= -1; }
-
-int s21_sum(s21_decimal *dst1, s21_decimal *dst2) {
-  long subsum1 = dst1->bits[0] + dst2->bits[0];
-  long subsum2 = dst1->bits[1] + dst2->bits[1];
-  long subsum3 = dst1->bits[2] + dst2->bits[2];
-  if (subsum1 > __INT_MAX__) subsum2 += 1;
-  if (subsum2 > __INT_MAX__) subsum3 += 1;
-  return 0;
+int s21_negate(s21_decimal value, s21_decimal *result) {
+  decimal_zero(result);
+  invert_sign(&value);
+  int code = 0;  // ??
+  for (int i = 0; i < 4; i++) result->bits[i] = value.bits[i];
+  return code;
 }
--- s21_decimal.h
+++ s21_decimal.h
@@ -4,12 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MASK 0x80000000
+
 typedef struct {
   int bits[4];
 } s21_decimal;
 
-void s21_decimal_zero(s21_decimal *dst);
+int get_sign(s21_decimal dst);
+int get_bit(s21_decimal dst, unsigned pos);
+void invert_sign(s21_decimal *dst);
+void decimal_zero(s21_decimal *dst);
 int s21_from_int_to_decimal(int src, s21_decimal *dst);
-void s21_invert_sign(s21_decimal *dst);
+int s21_negate(s21_decimal value, s21_decimal *result);
 
 #endif
