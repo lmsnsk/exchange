9b458177493daf3be1fd825a6a440721ed592deb
glengunt <lmslms87@yandex.ru>
2024-01-03 16:30:00 +0700

    5555555

--- compare_func.c
+++ compare_func.c
@@ -18,9 +18,36 @@ int big_is_greater(big_dec value_1, big_dec value_2) {
   return compare;
 }
 
+int compare(s21_decimal value_1, s21_decimal value_2) {
+  int compare = 0;
+  big_dec big_val_1 = from_decimal_to_big_decimal(value_1);
+  big_dec big_val_2 = from_decimal_to_big_decimal(value_2);
+  int sign_1 = get_sign(value_1);
+  int sign_2 = get_sign(value_2);
+
+  if (!sign_1 && sign_2) {
+    compare = 1;
+  } else if (sign_1 && !sign_2) {
+    compare = -1;
+  } else {
+    int scale_1 = get_scale(value_1);
+    int scale_2 = get_scale(value_2);
+
+    if (scale_1 > scale_2)
+      big_normalization(&big_val_2, scale_1 - scale_2);
+    else if (scale_1 < scale_2)
+      big_normalization(&big_val_1, scale_2 - scale_1);
+
+    compare = big_is_greater(big_val_1, big_val_2);
+    if (sign_1) compare *= -1;
+  }
+  return compare;
+}
+
 int is_greater_or_not(s21_decimal value_1, s21_decimal value_2, int *result,
                       int *check, int sign_1, int val) {
   int compare = 0;
+
   big_dec big_val_1 = from_decimal_to_big_decimal(value_1);
   big_dec big_val_2 = from_decimal_to_big_decimal(value_2);
   int sign_2 = get_sign(value_2);
@@ -77,75 +104,87 @@ int is_equal_or_not(s21_decimal value_1, s21_decimal value_2, int val) {
 }
 
 int s21_is_greater(s21_decimal value_1, s21_decimal value_2) {
-  int result, check = 0;
-  int sign_1 = get_sign(value_1);
-  int compare = is_greater_or_not(value_1, value_2, &result, &check, sign_1, 0);
-  if (check) {
-    if (!sign_1)
-      result = compare > 0 ? 1 : 0;
-    else
-      result = compare >= 0 ? 0 : 1;
-  }
+  // int result, check = 0;
+  // int sign_1 = get_sign(value_1);
+  // int compare = is_greater_or_not(value_1, value_2, &result, &check, sign_1,
+  // 0);
+  // if (check) {
+  //   if (!sign_1)
+  //     result = compare > 0 ? 1 : 0;
+  //   else
+  //     result = compare >= 0 ? 0 : 1;
+  // }
+  int comp = compare(value_1, value_2);
+  int result = comp == 1 ? 1 : 0;
   return result;
 }
 
 int s21_is_greater_or_equal(s21_decimal value_1, s21_decimal value_2) {
-  int result;
-  if (is_decimal_zero(value_1) && is_decimal_zero(value_2)) {
-    result = 1;
-  } else {
-    int check = 0;
-    int sign_1 = get_sign(value_1);
-    int compare =
-        is_greater_or_not(value_1, value_2, &result, &check, sign_1, 0);
-    if (check) {
-      if (!sign_1)
-        result = compare >= 0 ? 1 : 0;
-      else
-        result = compare > 0 ? 0 : 1;
-    }
-  }
+  // int result;
+  // if (is_decimal_zero(value_1) && is_decimal_zero(value_2)) {
+  //   result = 1;
+  // } else {
+  //   int check = 0;
+  //   int sign_1 = get_sign(value_1);
+  //   int compare =
+  //       is_greater_or_not(value_1, value_2, &result, &check, sign_1, 0);
+  //   if (check) {
+  //     if (!sign_1)
+  //       result = compare >= 0 ? 1 : 0;
+  //     else
+  //       result = compare > 0 ? 0 : 1;
+  //   }
+  // }
+  int comp = compare(value_1, value_2);
+  int result = comp >= 0 ? 1 : 0;
   return result;
 }
 
 int s21_is_less(s21_decimal value_1, s21_decimal value_2) {
-  int result, check = 0;
-  int sign_1 = get_sign(value_1);
-  int compare = is_greater_or_not(value_1, value_2, &result, &check, sign_1, 1);
-  if (check) {
-    if (!sign_1)
-      result = compare < 0 ? 1 : 0;
-    else
-      result = compare <= 0 ? 0 : 1;
-  }
+  // int result, check = 0;
+  // int sign_1 = get_sign(value_1);
+  // int compare = is_greater_or_not(value_1, value_2, &result, &check, sign_1,
+  // 1); if (check) {
+  //   if (!sign_1)
+  //     result = compare < 0 ? 1 : 0;
+  //   else
+  //     result = compare <= 0 ? 0 : 1;
+  // }
+  int comp = compare(value_1, value_2);
+  int result = comp == -1 ? 1 : 0;
   return result;
 }
 
 int s21_is_less_or_equal(s21_decimal value_1, s21_decimal value_2) {
-  int result;
-  if (is_decimal_zero(value_1) && is_decimal_zero(value_2)) {
-    result = 1;
-  } else {
-    int check = 0;
-    int sign_1 = get_sign(value_1);
-    int compare =
-        is_greater_or_not(value_1, value_2, &result, &check, sign_1, 1);
-    if (check) {
-      if (!sign_1)
-        result = compare <= 0 ? 1 : 0;
-      else
-        result = compare < 0 ? 0 : 1;
-    }
-  }
+  // int result;
+  // if (is_decimal_zero(value_1) && is_decimal_zero(value_2)) {
+  //   result = 1;
+  // } else {
+  //   int check = 0;
+  //   int sign_1 = get_sign(value_1);
+  //   int compare =
+  //       is_greater_or_not(value_1, value_2, &result, &check, sign_1, 1);
+  //   if (check) {
+  //     if (!sign_1)
+  //       result = compare <= 0 ? 1 : 0;
+  //     else
+  //       result = compare < 0 ? 0 : 1;
+  //   }
+  // }
+  int comp = compare(value_1, value_2);
+  int result = comp <= 0 ? 1 : 0;
   return result;
 }
 
 int s21_is_equal(s21_decimal value_1, s21_decimal value_2) {
-  int result = is_equal_or_not(value_1, value_2, 0);
+  // int result = is_equal_or_not(value_1, value_2, 0);
+  int result = compare(value_1, value_2);
+  result = result ? 0 : 1;
   return result;
 }
 
 int s21_is_not_equal(s21_decimal value_1, s21_decimal value_2) {
-  int result = is_equal_or_not(value_1, value_2, 1);
+  int result = compare(value_1, value_2);
+  result = result ? 1 : 0;
   return result;
 }
